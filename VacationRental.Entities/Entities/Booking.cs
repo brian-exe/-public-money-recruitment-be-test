813906ceeb19f10c2d995e67917956de7a80ebb3
Ezequiel Aciar <ez_aciar@farmacity.com.ar>
2022-07-15 01:11:57 -0300

    Intersection and End calculation added to Booking

--- VacationRental.Entities/Entities/Booking.cs
+++ VacationRental.Entities/Entities/Booking.cs
@@ -9,9 +9,13 @@ namespace VacationRental.Domain.Entities
         public int RentalId { get; set; }
         public DateTime Start { get; set; }
         public int Nights { get; set; }
+        public DateTime End() => Start.Date.AddDays(Nights);
 
         public bool IsDateBooked(DateTime dateToCheck)
             => dateToCheck >= Start && dateToCheck <= Start.AddDays(Nights);
+
+        public bool IntersectsWithRange(DateTime start, DateTime end) => (Start <= end) && (End() >= start);
+
 
     }
 }
